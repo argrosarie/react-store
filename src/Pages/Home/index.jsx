78e19ea41fde675f25be4eb96e5141ebdc00fad0
rosarie <rosariosanchezsampietro@gmail.com>
2023-05-26 16:24:28 -0300

    Card Component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,10 @@
 import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 export default function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      Home
+      <Card />
+    </Layout>
+  );
 }
