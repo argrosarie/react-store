452b462884b491f4d71c61cb3f46dec6cae4f703
rosarie <rosariosanchezsampietro@gmail.com>
2023-06-04 14:44:20 -0300

    Fetching API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 export default function Home() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-3 grid-cols-4 w-full max-w-screen-lg">
+        {" "}
+        {items?.map((item, idx) => {
+          return <Card key={idx} data={item} />;
+        })}
+      </div>
     </Layout>
   );
 }
